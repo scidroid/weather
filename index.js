99e5121102d636e7dbb30e9f622816f570afbf2b
SciDroid <scidroidgames@gmail.com>
2021-04-24 21:35:56 -0500

    Added UI and temepraure, but not work ok. i fix this tomorrow.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const fetchData = () => {
   window
     .fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${
-        document.getElementById("city").value.split(' ').join('+')
-      }&appid=6edcd89e1bf91ff50e8852c2a5b89a45`
+      `https://api.openweathermap.org/data/2.5/weather?q=${document
+        .getElementById("city")
+        .value.split(" ")
+        .join("+")}&appid=6edcd89e1bf91ff50e8852c2a5b89a45`
     )
     .then((response) => response.json())
     .then((responseJSON) => {
-        const container = document.querySelector(".results");
+      const container = document.querySelector(".results");
       if (responseJSON.cod !== 200) {
         console.log("falló");
         const error = document.createElement("h4");
-        error.textContent = "Sorry, a wild error has apared, Try Again.";
+        error.textContent =
+          "Sorry, a wild error has apared, check the spell and try again.";
         error.className = "error";
         container.append(error);
       } else {
@@ -19,8 +21,29 @@ const fetchData = () => {
         const cityFromResponse = document.createElement("h4");
         cityFromResponse.textContent = `Found ${responseJSON.name}, ${responseJSON.sys.country}`;
         cityFromResponse.className = "found";
-        container.append(cityFromResponse);
+        //i need to fix this tomorrow.
+        //const image = document.createElement("img");
+        //image.src = `https://openweathermap.org/img/w/${responseJSON.weather[3]}.png`;
+        //const description = document.createElement("h5");
+        //console.log(responseJSON.weather[2]);
+        //description.textContent = responseJSON.weather[2];
+        const temp = document.createElement("p");
+        temp.textContent = `Min. Temperature ${parseFloat(
+          responseJSON.main.temp_min / 33.8
+        ).toFixed(2)}C°, Max. Temperature ${parseFloat(
+          responseJSON.main.temp_max / 33.8
+        ).toFixed(2)}C°`;
+        container.append(cityFromResponse, temp);
       }
+    })
+    .catch((err) => {
+      console.error(err);
+      const container = document.querySelector(".results");
+      const error = document.createElement("h4");
+      error.textContent =
+        "Sorry, we have a problem to connect you with the API, check your connecction and try again.";
+      error.className = "errorAPI";
+      container.append(error);
     });
 };
 
